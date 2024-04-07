ce212c234e29731190fa441e29e9e9cf6afdf444
turik777 <52492191+turik777@users.noreply.github.com>
2024-04-08 00:59:35 +0300

    Add proper height and width to the divs

--- main.js
+++ main.js
@@ -3,6 +3,8 @@ const container = document.querySelector(".container");
 for (let i = 0; i < 16 * 16; i++) {
     const div = document.createElement("div");
     container.appendChild(div);
+    div.style.height = "60px";
+    div.style.width = div.style.height;
     div.addEventListener("mouseover", () => {
         div.style.backgroundColor = "#000000";
     });
