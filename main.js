9abe01d8ca6fc5f9dcf0a0cce1a50e0c7e1b5eaf
turik777 <52492191+turik777@users.noreply.github.com>
2024-04-08 00:47:26 +0300

    Add hover effect

--- main.js
+++ main.js
@@ -3,4 +3,7 @@ const container = document.querySelector(".container");
 for (let i = 0; i < 16 * 16; i++) {
     const div = document.createElement("div");
     container.appendChild(div);
+    div.addEventListener("mouseover", () => {
+        div.style.backgroundColor = "#000000";
+    });
 }
