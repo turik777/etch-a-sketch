9400188062bbae2b5ae457c9db6b59c0ba53eaa3
turik777 <52492191+turik777@users.noreply.github.com>
2024-04-08 02:26:17 +0300

    Add function for random color grid

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 const container = document.querySelector(".container");
-const button = document.querySelector("button");
+const black = document.querySelector("#black");
+const color = document.querySelector("#color");
 
 for (let i = 0; i < 16 * 16; i++) {
     const div = document.createElement("div");
@@ -11,7 +12,8 @@ for (let i = 0; i < 16 * 16; i++) {
     });
 }
 
-button.addEventListener("click", newGrid);
+black.addEventListener("click", newGrid);
+color.addEventListener("click", newColorGrid);
 
 function newGrid(squaresPerSide) {
     squaresPerSide = prompt("Enter number of squares per side:", 16);
@@ -30,6 +32,27 @@ function newGrid(squaresPerSide) {
         div.style.width = div.style.height;
         div.addEventListener("mouseover", () => {
             div.style.backgroundColor = "#000000";
+        });
+    }
+}
+
+function newColorGrid(squaresPerSide) {
+    squaresPerSide = prompt("Enter number of squares per side:", 16);
+    if (squaresPerSide > 100) {
+        alert("Maximum number is 100");
+        return;
+    }
+    const div = document.querySelectorAll(".container div");
+    div.forEach(square => {
+        container.removeChild(square);
+    })
+    for (let i = 0; i < squaresPerSide * squaresPerSide; i++) {
+        const div = document.createElement("div");
+        container.appendChild(div);
+        div.style.height = 960 / squaresPerSide + "px";
+        div.style.width = div.style.height;
+        div.addEventListener("mouseover", () => {
+            div.style.backgroundColor = "#" + Math.floor(Math.random() * 16777215).toString(16);
         });
     }
 }
